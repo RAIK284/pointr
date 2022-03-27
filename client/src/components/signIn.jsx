78167f1d57d856d6af5d32ec1547c3aef9bbe10e
shivanimudhelli <69917335+shivanimudhelli@users.noreply.github.com>
2022-03-26 23:12:25 -0500

    changing styles
    added:
    - gradient background
    - centered log in box
    - input boxes
    
    still need:
    - move button into box
    - fix fonts

--- client/src/components/signIn.jsx
+++ client/src/components/signIn.jsx
@@ -8,18 +8,23 @@ class SignIn extends Component {
     render() {
         return (
             <React.Fragment >
-                Sign In
-                <body className="signIn">
-                    Username:
-                    <br />
-                    <input type="text" placeholder="Input Username Here"></input>
-                    <br />
-                    Password:
-                    <br />
-                    <input type="text" placeholder="Input Password Here"></input>
-                    <br />
-                    <br />
-                    <Button id="button" variant="contained" size="large" onClick={()=>{window.location.href='/profile'}}>Login</Button>
+                
+                <body id="SI">
+                    <style>
+                        @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;700&display=swap');
+                        </style>
+                    <div className="signIn">
+                        
+                        <br />
+                        <input type="text" placeholder="Input Username Here" className="br1"></input>
+                        <br />
+                        
+                        <br />
+                        <input type="text" placeholder="Input Password Here" className="br2"></input>
+                        <br />
+                        <br />
+                        <Button className="button" variant="contained" size="large" onClick={()=>{window.location.href='/profile'}}>Login</Button>
+                    </div>
                 </body>
             </React.Fragment>
         );
@@ -30,5 +35,6 @@ class SignIn extends Component {
 //     field1: PropTypes.string.isRequired,
 //     field2: PropTypes.string.isRequired,
 // };
+
 
 export default SignIn;
