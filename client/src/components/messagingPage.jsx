a5b6f524ff7e519adedda10c38c8ca8ac2905fb1
pvasudevan2 <69927853+pvasudevan2@users.noreply.github.com>
2022-04-02 01:14:41 -0500

    fix messages to display horizontally using flexbox properties

--- client/src/components/messagingPage.jsx
+++ client/src/components/messagingPage.jsx
@@ -17,23 +17,15 @@ class MessagingPage extends Component {
                     new message
                 </Button>
 
-                {/* <div> */}
-                    <MessageDisplayBox id="box1">
-                        
-                    </MessageDisplayBox>
-                {/* </div> */}
+                <div id="messageDisplayBoxes">
 
-                {/* <div> */}
-                    <MessageDisplayBox id="box2">
-                        
-                    </MessageDisplayBox>
-                {/* </div> */}
+                    <MessageDisplayBox id="box1"> </MessageDisplayBox>
+            
+                    <MessageDisplayBox id="box2"> </MessageDisplayBox>
+ 
+                    <MessageDisplayBox id="box3"></MessageDisplayBox>
 
-                {/* <div> */}
-                    <MessageDisplayBox id="box3">
-
-                    </MessageDisplayBox>
-                {/* </div> */}
+                </div>
 
 
 
