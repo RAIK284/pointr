61136a6791a73ee1de1ad68a5c45541eda54e77a
shivanimudhelli <69917335+shivanimudhelli@users.noreply.github.com>
2022-04-05 22:26:52 -0500

    creating trophy object

---
+++ client/src/components/trophy.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import './styles/trophy.css';
+import Button from '@mui/material/Button';
+
+class Trophy extends Component {
+    render() {
+        return (
+            <React.Fragment >
+                
+                
+
+            </React.Fragment>
+        );
+
+    }   
+}
+export default Trophy;
