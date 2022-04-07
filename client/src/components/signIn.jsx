a54a71d77ed86d99546a19db2539b95edc124403
Samuel I <saingledue@outlook.com>
2022-04-07 16:14:09 -0500

    "Fixed" the merge main dilemma

--- client/src/components/signIn.jsx
+++ client/src/components/signIn.jsx
@@ -1,11 +1,33 @@
 import React, { Component } from "react";
 import './styles/signIn.css';
 import Button from '@mui/material/Button';
+import {validateUser} from './scripts/signInValidation.js';
 //import Stack from '@mui/material/Stack';
 //import PropTypes from 'prop-types';
 
 class SignIn extends Component {
     render() {
+
+        const checkresponse = async (response) => {
+            console.log(response)
+            if (response !== -1) {
+                //Switch to profile page.
+                window.location.href='/profile'
+                console.log("Successul!");
+                console.log(response);
+            } else {
+                //Display error and do nothing.
+                console.log("Wrong!");
+                console.log(response);
+            }
+            window.location.href="/profile"
+        }
+
+        const changePage = async () =>
+        {
+            validateUser().then(res => checkresponse(res));
+        }
+
         return (
             <React.Fragment >
                 
@@ -15,14 +37,14 @@ class SignIn extends Component {
                             Log In to Pointr
                         </text>
                         <br />
-                        <input type="text" placeholder="Username" className="br1"></input>
+                        <input type="text" placeholder="Username" id="email" className="br1"></input>
                         <br />
                         
                         <br />
-                        <input type="text" placeholder="Password" className="br2"></input>
+                        <input type="text" placeholder="Password" id="password" className="br2"></input>
                         <br />
                         <br />
-                        <Button variant="contained" size="large" id= "loginbutton" onClick={()=>{window.location.href='/profile'}}>Login</Button>
+                        <Button variant="contained" size="large" id= "loginbutton" onClick={()=>{window.location.href="/profile"}}>Login</Button>
                     </div>
                 </body>
             </React.Fragment>
