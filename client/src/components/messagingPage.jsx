3b00ffe72b81b9c6fc2da519ea6e58bd3d40b948
Blake Simpleman <40041217+bsimpleman@users.noreply.github.com>
2022-04-07 15:30:08 -0500

    added fetch for messages

--- client/src/components/messagingPage.jsx
+++ client/src/components/messagingPage.jsx
@@ -6,7 +6,27 @@ import InternalHeading from "./internalHeading";
 import HeaderDrawer from "./headerDrawer";
 
 class MessagingPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+
+    async componentDidMount() {
+        await fetch('http://localhost:8080/api/message?receiver=bsimpleman')
+            .then(response => response.json())
+            .then(data => this.setState({data}))
+    }
+
     render() {
+        if(this.state.data === undefined) return <div>Loading...</div>
+        console.log(this.state.data)
+        let messages = [];
+
+        console.log()
+
+        for (let i = 0; i < this.state.data.length; i++) {
+            messages.push(<MessageDisplayBox name={this.state.data[i].name} username={this.state.data[i].sender} messageBody={this.state.data[i].messageBody}></MessageDisplayBox>);
+        }
         return (
             <React.Fragment >
                 <div id="messagingBackground">
