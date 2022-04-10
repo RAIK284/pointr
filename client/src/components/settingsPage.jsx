63ceb4dbe93de93d247de3ac509deebafe384d56
Blake Simpleman <40041217+bsimpleman@users.noreply.github.com>
2022-04-09 22:06:30 -0500

    small fix for isPrivate checkbox

--- client/src/components/settingsPage.jsx
+++ client/src/components/settingsPage.jsx
@@ -112,7 +112,7 @@ class SettingsPage extends Component {
                             {/* This will be a logout button. <Button variant="contained" size="large" onClick={}>Logout</Button>*/}
                         </div>
                         <div id={"bottom-options"}>
-                            <input type="checkbox"  className="settings-checkbox" name="privacy" checked={this.state.isPrivate} onChange={this.handleChange} onClick={ () => { this.setState({ isPrivate: !this.state.isPrivate })}} required></input>
+                            <input type="checkbox"  className="settings-checkbox" name="privacy" checked={!this.state.isPrivate} onChange={this.handleChange} onClick={ () => { this.setState({ isPrivate: !this.state.isPrivate })}} required></input>
                             <label htmlFor="privacy" className="checkboxLabel">Display my information publicly</label>
                             <p id={"privacy-info"}>
                                 Enabling this will allow other users to see your tropies, FUNDs, and leaderboard rank.
