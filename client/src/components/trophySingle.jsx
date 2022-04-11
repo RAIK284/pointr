d564405dfb8a482fe8dd43434aee8263c6ae9c44
shivanimudhelli <69917335+shivanimudhelli@users.noreply.github.com>
2022-04-10 23:41:51 -0500

    adding the single display

---
+++ client/src/components/trophySingle.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import './styles/trophy.css';
+import Button from '@mui/material/Button';
+import ducky from './images/ducky.png'
+
+
+class TrophySingle extends Component {
+
+    render() {
+        return (
+            <React.Fragment >
+                    
+                
+                <div className="storeDisplay">
+                     
+                    <img src={ducky} id="trophyImageSmall"/>
+
+                    <div id="trophyCostSmall">
+                        <text>100 fds</text> 
+                    </div>
+
+
+                </div>
+                
+            
+
+            </React.Fragment>
+        );
+
+    }   
+    
+}
+export default TrophySingle;
