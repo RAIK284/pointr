cc12908d461fa462b2b93399e8a68a661ecfbfab
Blake Simpleman <40041217+bsimpleman@users.noreply.github.com>
2022-04-12 15:13:02 -0500

    message functionality

--- client/src/components/newMessage.jsx
+++ client/src/components/newMessage.jsx
@@ -1,27 +1,77 @@
-import React, { Component } from "react";
+import React, { Component, useState} from "react";
 import './styles/newMessage.css';
 import Button from '@mui/material/Button';
 
 function NewMessage(props){
+
+    const [email, setEmail] = useState('');
+    const [messageBody, setMessageBody] = useState('');
+
+    const messageObject = {
+        "timestamp": "",
+        "sender": "bsimpleman",
+        "receiver": "",
+        "value": "",
+        "messageBody": "",
+        "isRead": false
+    }
+
+    const handleInput = () => {
+        setMessageData()
+    }
+
+    const getMessageValue = () => {
+
+    }
+
+    const setMessageData = async () => {
+        getMessageValue();
+        messageObject.timestamp = new Date().toUTCString()
+        messageObject.receiver = email;
+        messageObject.messageBody = messageBody;
+
+        console.log(messageBody)
+        await postMessage(messageObject);
+        await subtractMessageValue(messageObject);
+    }
+
+    const postMessage = async (messageObject) => {
+        const jsonData = JSON.stringify(messageObject);
+
+        fetch("http://localhost:8080/api/message", {
+            method: "POST",
+            headers: {'Content-Type': 'application/json'},
+            body: jsonData
+        });
+    }
+
+    const subtractMessageValue = async (messageObject) => {
+        const jsonData = JSON.stringify(messageObject);
+
+        fetch("http://localhost:8080/api/user?username=bsimpleman", {
+            method: "PATCH",
+            headers: {'Content-Type': 'application/json'},
+            body: jsonData
+        });
+    }
+
     return(props.trigger) ? (
         <React.Fragment >
 
             <div className = 'newMessage'>
                 <div className = 'newMessageInner'>
-                    
                     <text className = 'sendTo'>Send a message to: </text>
-                    
-                    <input className = 'sendToEntryBox' type="text" placeholder="send to email"></input>
+                    <input className = 'sendToEntryBox' name={"email"} onChange={e => setEmail(e.target.value)} type="text" placeholder="send to email"></input>
 
 
-                    <input className = 'messageEntryBox' type="text" placeholder="Type your message here..."></input>
+                    <input className = 'messageEntryBox' name={"messageBody"} type="text" onChange={e => setMessageBody(e.target.value)} placeholder="Type your message here..."></input>
 
 
                     <div className = 'tokensBox'>
                         [emojis will go here]
                     </div>
 
-                    <Button variant="text" id= "send">
+                    <Button variant="text" id= "send" onClick={() => handleInput()}>
                         Send
                     </Button>
 
