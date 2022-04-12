40d15fb1e47c3fcd161f0e8543e61fb447e9747f
Samuel I <saingledue@outlook.com>
2022-04-12 16:00:55 -0500

    Added a searchbar and some styles to the explorePage

--- client/src/components/explorePage.jsx
+++ client/src/components/explorePage.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import HeaderDrawer from "./headerDrawer.jsx";
 //reference: https://mui.com/components/drawers/
+import "./styles/explorePage.css";
+import searchicon from "./images/search.svg"
 
 class ExplorePage extends Component {
     render() {
@@ -9,6 +11,14 @@ class ExplorePage extends Component {
                 
             <HeaderDrawer index={3}></HeaderDrawer>
                 <div id="content">
+                    <p className="header">Explore</p>
+                    <input type="text" placeholder="Search for users..." className="search"></input>
+                    <img id="img" alt="search" src={searchicon}/>
+                    <br />
+                    <br />
+                    <p>This should be a loop component for displaying
+                        profiles!
+                    </p>
                 </div>
             </React.Fragment>
         );
