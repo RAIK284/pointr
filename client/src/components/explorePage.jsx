7bab87db0d97a7027d3ea6f0c67980436626ccd8
shivanimudhelli <69917335+shivanimudhelli@users.noreply.github.com>
2022-04-13 10:59:21 -0500

    Temporary comment

--- client/src/components/explorePage.jsx
+++ client/src/components/explorePage.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import HeaderDrawer from "./headerDrawer.jsx";
 import SearchBar from "./searchBar.jsx";
-import './styles/explorePage.css';
+/* import './styles/explorePage.css'; */
 //reference: https://mui.com/components/drawers/
 
 class ExplorePage extends Component {
