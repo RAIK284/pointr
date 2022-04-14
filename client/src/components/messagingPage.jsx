fabed10fd53ea2f3fe6356a8e7b7435114ae899c
Blake Simpleman <40041217+bsimpleman@users.noreply.github.com>
2022-04-14 16:58:17 -0500

    Added the ability to toggle between sent/received messages

--- client/src/components/messagingPage.jsx
+++ client/src/components/messagingPage.jsx
@@ -12,8 +12,15 @@ class MessagingPage extends Component {
         this.state = {
             receivedMessages: {},
             sentMessages: {},
-            newMessage: false
+            newMessage: false,
+            viewStatus: false
         }
+    }
+
+    handleChange() {
+        const newStatus = !this.state.viewStatus;
+        this.setState({viewStatus: newStatus});
+        console.log(this.state.viewStatus);
     }
 
      componentDidMount() {
@@ -40,39 +47,75 @@ class MessagingPage extends Component {
             receivedMessages.push(<MessageDisplayBox name={this.state.receivedMessages[i].name} username={this.state.receivedMessages[i].sender} messageBody={this.state.receivedMessages[i].messageBody}></MessageDisplayBox>);
         }
 
-        return (
-            <React.Fragment >
-                <div id="messagingBackground">
+        if (this.state.viewStatus) {
+            return (
+                <React.Fragment >
+                    <div id="messagingBackground">
 
-                    <HeaderDrawer index={1}></HeaderDrawer>
-                  
-                    <InternalHeading title="Messages"></InternalHeading>
+                        <HeaderDrawer index={1}></HeaderDrawer>
 
-                    <Button variant="text" id= "newMessage" onClick={()=> this.setState({newMessage: true})}>
-                        new message
-                    </Button>
+                        <InternalHeading title="Messages"></InternalHeading>
 
-                    <label className="toggle">
-                        <input type="checkbox"></input>
-                        <span className="labels" data-on="Sent" data-off="Recieved"></span>
-                    </label>
+                        <Button variant="text" id= "newMessage" onClick={()=> this.setState({newMessage: true})}>
+                            new message
+                        </Button>
 
-                    <div id="messageDisplayBoxes">
-                        {/* The way this is displaying is NOT how it should be. We should have a call
+                        <label className="toggle">
+                            <input type="checkbox" onChange={() => this.handleChange()}></input>
+                            <span className="labels" data-on="Sent" data-off="Recieved"></span>
+                        </label>
+
+                        <div id="messageDisplayBoxes">
+                            {/* The way this is displaying is NOT how it should be. We should have a call
                         to the api, and loop throug every message. Then, we display a box for every message. That way
                         for ex if someone only has 2 messages only 2 boxes show up */}
-                        {sentMessages}
+                            {sentMessages}
+                        </div>
+
                     </div>
 
-                </div>
+                    <NewMessage trigger={this.state.newMessage}>
+                        <button className = 'closeButton' onClick={()=> this.setState({newMessage: false})}>
+                            X
+                        </button>
+                    </NewMessage>
+                </React.Fragment>
+            );
+        } else {
+            return (
+                <React.Fragment >
+                    <div id="messagingBackground">
 
-                <NewMessage trigger={this.state.newMessage}>
-                    <button className = 'closeButton' onClick={()=> this.setState({newMessage: false})}>
-                        X
-                    </button>
-                </NewMessage>
-            </React.Fragment>
-        );
+                        <HeaderDrawer index={1}></HeaderDrawer>
+
+                        <InternalHeading title="Messages"></InternalHeading>
+
+                        <Button variant="text" id= "newMessage" onClick={()=> this.setState({newMessage: true})}>
+                            new message
+                        </Button>
+
+                        <label className="toggle">
+                            <input type="checkbox" onChange={() => this.handleChange()}></input>
+                            <span className="labels" data-on="Sent" data-off="Recieved"></span>
+                        </label>
+
+                        <div id="messageDisplayBoxes">
+                            {/* The way this is displaying is NOT how it should be. We should have a call
+                        to the api, and loop throug every message. Then, we display a box for every message. That way
+                        for ex if someone only has 2 messages only 2 boxes show up */}
+                            {receivedMessages}
+                        </div>
+
+                    </div>
+
+                    <NewMessage trigger={this.state.newMessage}>
+                        <button className = 'closeButton' onClick={()=> this.setState({newMessage: false})}>
+                            X
+                        </button>
+                    </NewMessage>
+                </React.Fragment>
+            );
+        }
 
     }   
 }
