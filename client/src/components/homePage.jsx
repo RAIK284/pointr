d45a11cb00aee8d8088e8028fce60ca98758c8f6
shivanimudhelli <69917335+shivanimudhelli@users.noreply.github.com>
2022-04-18 11:59:54 -0500

    New home page (#32)
    * adding title and subtitle
    
    * adding info boxes
    
    * info box positioning
    
    not completely done; still need to evenly space
    
    * center aligning text in info boxes
    
    first box doesn't look right yet
    
    * home page elements added
    
    still need to position everything properly
    
    * columns fixed
    
    * colors of content boxes
    
    * sign up button
    
    * adding icon to div
    
    * adding images to the info boxes
    
    * center align text and info box
    
    * removing temp border
    
    * removing temp change

--- client/src/components/homePage.jsx
+++ client/src/components/homePage.jsx
@@ -3,11 +3,125 @@ import { MuiNavbar, NavItem } from 'mui-navbar';
 import './styles/homePage.css';
 import Button from '@mui/material/Button';
 import homePagePic from "./images/header-icons/homePagePic.jpeg";
+import { color } from "@mui/system";
+import msgIcon from "./images/header-icons/message.svg";
+import trophyIcon from "./images/header-icons/leaderboard.svg";
+import starIcon from "./images/face-grin-stars.svg";
 class HomePage extends Component {
     render() {
         return (
             <React.Fragment>
-                <MuiNavbar
+               
+
+                    <MuiNavbar
+                        logoText="Pointr"
+                        background="#0064FF"
+                        navItemPosition="right"
+                        InputLabelProps={{ style: { fontFamily:'Poppins', fontWeight: '700px',borderWidth:'3px'}  }}
+
+                    >
+                        <NavItem to="/">Contact</NavItem>
+                        <NavItem to="/">About</NavItem>
+                        <NavItem to="/signin">Log In</NavItem>
+                    </MuiNavbar> 
+
+                    <div id="titleHeader">
+                        <div id="homeTitleWrapper">
+                            <text id="homeTitle">Welcome to Pointr.</text>
+                        </div>
+                        <div id="homeSubtitleWrapper">
+                            <text id="homeSubtitle">The new way to be positive</text>
+                        </div>
+
+                        <div id="homeButtonWrapper">
+                        <Button id ="homepagebutton" variant="contained" size="large" onClick={()=>{window.location.href='/signup'}}>Sign Up</Button>
+                        </div>
+
+                    </div>
+
+                    <div id="rightBoxes">
+                            <div className="infoBoxWrapper">
+                                <div className="infoBox" style={{background: "#66D8C1"}}>
+                                    <img src={msgIcon} className="icon" id="icon1"></img>
+                                </div>
+                                <div className="captionLower">Send a motivational note.</div>
+                            </div>
+
+                            <div className="infoBoxWrapper">
+                            <div className="captionUpper">Earn points for your positivity.</div>
+                                <div className="infoBox" style={{background: "#03B795"}}> 
+                                    <img src={trophyIcon} className="icon" id="icon2"></img>
+                                </div>
+                                
+                            </div>
+
+
+                            <div className="infoBoxWrapper">
+                                <div className="infoBox" style={{background: "#0064FF"}}>
+                                    <img src={starIcon} className="icon" id="icon3"></img>
+                                </div>
+                                <div className="captionLower">Buy trophies to inspire others!</div>
+
+                            </div>
+                    </div>
+                    
+{/*                 <div class="flex-container">
+                    <div id="rightBoxes">
+
+                        <div className="infoBoxWrapperLeft">
+                            <text className="infoBox">
+                                Send a motivational note.
+                            </text>
+                        </div>
+
+                        <div className="infoBoxWrapperRight"> 
+                            <text className="infoBox">
+                                Earn points for your positivity.
+                            </text>
+                        </div>
+
+                        <div className="infoBoxWrapperLeft">
+                            <text className="infoBox">
+                                Buy trophies to inspire others!
+                            </text>
+                        </div>
+
+                    </div>
+                
+                <div id="leftContent">
+                    <div >
+                        <text id="easyText">It's easy:</text>
+                    </div>
+
+                    <div>
+                        <text id="startedText">Get started today.</text>
+                    </div>
+
+                    <div>
+                        <Button id ="homepagebutton" variant="contained" size="large" onClick={()=>{window.location.href='/signup'}}>Sign Up</Button>
+                    </div>
+
+                </div>
+            </div>  */}  
+
+
+                
+            </React.Fragment>
+                
+        );
+    }
+}
+
+// SignIn.propTypes = {
+//     field1: PropTypes.string.isRequired,
+//     field2: PropTypes.string.isRequired,
+// };
+
+export default HomePage;
+
+
+{/* <React.Fragment>
+<MuiNavbar
                     logoText="Pointr"
                     background="#0064FF"
                     navItemPosition="right"
@@ -30,14 +144,4 @@ class HomePage extends Component {
                 </div>
                 
                 
-            </React.Fragment>
-        );
-    }
-}
-
-// SignIn.propTypes = {
-//     field1: PropTypes.string.isRequired,
-//     field2: PropTypes.string.isRequired,
-// };
-
-export default HomePage;
+            </React.Fragment> */}
