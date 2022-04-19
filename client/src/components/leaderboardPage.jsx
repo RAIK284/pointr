29fb1caa9cdb1d0dc0506c91185f91c878220b79
shivanimudhelli <69917335+shivanimudhelli@users.noreply.github.com>
2022-04-19 08:44:49 -0500

    formatting the username on the top 3 cards

--- client/src/components/leaderboardPage.jsx
+++ client/src/components/leaderboardPage.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import HeaderDrawer from "./headerDrawer.jsx";
 import InternalHeading from "./internalHeading.jsx";
 import './styles/leaderboardPage.css';
+import duck from "./images/ducky.png";
 
 
 //reference: https://mui.com/components/drawers/
@@ -15,20 +16,36 @@ class LeaderBoardPage extends Component {
 
                 
             <HeaderDrawer index={4}></HeaderDrawer>
-            <div id="leaderboardBackground">
-            <div id="fundsAmountWrapper">
-                    <text id="fundsAmountText">Your all-time FUNds:</text>
-                    <var id="fundsAmountNum">20,000</var>
-                </div>
+            <div id="lbBackground">
+                <div id="fundsAmountWrapper">
+                        <text id="fundsAmountText">Your all-time FUNds:</text>
+                        <var id="fundsAmountNum">20,000</var>
+                    </div>
 
-                <div id="topThreeDisplay">
-                    
-                </div>
+                    <div id="topThreeDisplay">
+                        <div className="topThreeSingle">
+                            <img src={duck} className="topProfilePic"></img>
+                            <text className="topUsrnm" >@username</text>
+                        </div>
+                        <div className="topThreeSingle">
+                            <img src={duck} className="topProfilePic"></img>
+                            <text className="topUsrnm" >@username</text>
 
 
-                <div id="allDisplay">
-                    
-                </div>
+                        </div>
+
+                        <div className="topThreeSingle">
+                            <img src={duck} className="topProfilePic"></img>
+                            <text className="topUsrnm" >@username</text>
+
+                        </div>
+
+                    </div>
+
+
+                    <div id="allDisplay">
+                        
+                    </div>
                
             </div>
             </React.Fragment>
