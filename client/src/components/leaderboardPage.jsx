7077807f51b0358277159b7e5268c4fe76549f9f
pvasudevan2 <69927853+pvasudevan2@users.noreply.github.com>
2022-04-19 20:19:06 -0500

    Fix Leaderboard to only show rank 4-10 at bottom

--- client/src/components/leaderboardPage.jsx
+++ client/src/components/leaderboardPage.jsx
@@ -16,20 +16,14 @@ class LeaderBoardPage extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            tableData: [{ name: "@fake data", funds: 200, rank: 4 }, 
-            { name: "@fakeData2", funds: 150, rank: 5 },
-            { name: "@fakeData3", funds: 100, rank: 6}, { name: "@user3", funds: 90, rank: 7}, 
-            { name: "@fakeData4", funds: 80, rank: 8}, { name: "@user5", funds: 70, rank: 9}, { name: "@user6", funds: 60, rank: 10},]
+            tableData: [{ name: "", funds: 0, rank: 0 }, { name: "", funds: 0, rank: 0 },{ name: "", funds: 0, rank: 0}],// null/filler data
         }
     }
 
     fetchData = async () => {
         const response =  await fetch('http://localhost:8080/api/leaderboard?top=10')
         const data = await response.json();
-        console.log("HEREE")
-        console.log(data)
-        console.log(data[0].username)
-        console.log(data[0].allTimeFunds)
+
         this.setState({tableData: [{ name: data[0].username, funds: data[0].allTimeFunds, rank: 1}, 
         { name: data[1].username, funds: data[1].allTimeFunds, rank: 2}, 
         { name: data[2].username, funds: data[2].allTimeFunds, rank: 3}, 
@@ -41,16 +35,14 @@ class LeaderBoardPage extends Component {
         { name: data[8].username, funds: data[8].allTimeFunds, rank: 9}, 
         { name: data[9].username, funds: data[9].allTimeFunds, rank: 10}] 
         })
-
-        console.log("Now we are here")
-        console.log(this.state.tableData[0])
     };
     async componentDidMount() {
         // const tableData = [];
         await this.fetchData();// i added an await here... when we wait we get the right values
         // now the issue is when we render tableData[0] its not waiting to fetch data first
-        console.log("Inside Component did mount")
-        console.log(this.state.tableData[0])
+        console.log("Top 10 Users Fetched Results")
+        console.log(this.state.tableData)
+
     }
 
     render() {
@@ -98,11 +90,11 @@ class LeaderBoardPage extends Component {
                             <th>Username</th>
                             <th>FUNds</th> */}
                             </tr>
-                            {this.state.tableData.map((val, key) => { /* instead of tableData put in the list of usernames */
+                            {this.state.tableData.slice(3,).map((val, key) => {
                             return (
                                 <tr key={key}>
                                 <td id="rank">{val.rank}</td>
-                                <td id="name">{val.name}</td> { /* change "name" , "funds" , "rank" to be the variables set up in our database*/}
+                                <td id="name">{val.name}</td> 
                                 <td>{val.funds}</td>
                                 
                                 </tr>
