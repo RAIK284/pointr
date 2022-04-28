3650260fd94a8e320c87c1097348cbad48455801
Blake Simpleman <40041217+bsimpleman@users.noreply.github.com>
2022-04-28 16:16:04 -0500

    profile images

--- client/src/components/profileBox.jsx
+++ client/src/components/profileBox.jsx
@@ -2,9 +2,27 @@ import React, { Component } from "react";
 import ducky from "./images/ducky.png"
 import "./styles/profileBox.css"
 import PropTypes from 'prop-types';
+import blake from "./images/profile-pictures/blake.png"
+import wally from "./images/profile-pictures/wally.png"
+import prema from "./images/profile-pictures/prema-cropped.png"
+import shivani from "./images/profile-pictures/shivani.jpeg"
+import keck from "./images/profile-pictures/keck.jpg"
+import sam from "./images/profile-pictures/sam.png"
+
 
 class ProfileBox extends Component {
+
     render() {
+        const profileImages = {
+            "blake": blake,
+            "shivani": shivani,
+            "prema": prema,
+            "wally": wally,
+            "keck": keck,
+            "ducky": ducky,
+            "sam": sam
+        }
+
       return (
         <React.Fragment>
           <div>
@@ -12,13 +30,13 @@ class ProfileBox extends Component {
             (this.props.index % 2 === 0)
             ?
               <div className="box1" onClick={()=>{window.location.href="/user/" + this.props.otherProfile.username}}>
-                <img align="Left" id="image" alt="profilepic" src={ducky}/>
+                <img align="Left" id="image" alt="profilepic" src={profileImages[this.props.otherProfile.image]}/>
                 <p className="name">@{this.props.otherProfile.username}</p>
                 <p className="bio">{this.props.otherProfile.bio}</p>
               </div>
             :
             <div className="box2" onClick={()=>{window.location.href="/user/" + this.props.otherProfile.username}}>
-                <img align="Left" id="image" alt="profilepic" src={ducky}/>
+                <img align="Left" id="image" alt="profilepic" src={profileImages[this.props.otherProfile.image]}/>
                 <p className="name">@{this.props.otherProfile.username}</p>
                 <p className="bio">{this.props.otherProfile.bio}</p>
               </div>
