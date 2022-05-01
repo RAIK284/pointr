5eaefe5ad46352a0d7f194ccbcc94e9fc6286211
Blake Simpleman <40041217+bsimpleman@users.noreply.github.com>
2022-05-01 13:32:39 -0500

    package.json attempt 7

--- client/src/index.js
+++ client/src/index.js
@@ -16,10 +16,3 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static("build"));
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname,  "build", "index.html"));
-    });
-}
