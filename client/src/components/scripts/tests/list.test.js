8f51eb37cd64c54d05846532dacb3273a6567db5
tastybird107 <wholbeck2@huskers.unl.edu>
2022-05-04 10:27:22 -0500

    Two test fixes

--- client/src/components/scripts/tests/list.test.js
+++ client/src/components/scripts/tests/list.test.js
@@ -30,7 +30,7 @@ describe("<List />", () => {
     test('Input state of component is updated from user input', () => {
         const component = render(<List input = "bsimpleman"/>, container);
 
-        expect(container.props.input === ["bsimpleman"])
+        expect(container.input === ["bsimpleman"])
 
         //Need to reference "filteredData", as well as specify what the expected value should be.
     });
